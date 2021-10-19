36d784c62962aaf225349852df063ca3a4a0d4df
Salvador Martinez <cartoonrocket9@gmail.com>
2021-10-19 10:46:28 +0200

    adding constanst class

---
+++ marvel-API/marvel-API/src/main/java/com/Marvelchallenge/marvelAPI/Constants.java
@@ -0,0 +1,6 @@
+package com.Marvelchallenge.marvelAPI;
+
+public class Constants { //TODO: refactor api key & filters to be dynamic.
+    public static final String ApiUrlMain = "http://gateway.marvel.com/v1/public/";
+    public static final String ApiUrlCharacters = ApiUrlMain + "characters?orderBy=name&limit=100&ts=1&apikey=cccfd6b5fbfc8bc91c645fd3c39accaa&hash=d609be9bdc98ab7767176a5e3d9f1309";
+}
